96b0cfca7e43e453708ac293c7809bf0311c6761
indica <cancerosv3@gmail.com>
2026-02-07 18:17:57 +0800

    feat: create ProfilePage component for displaying user profile information

---
+++ app/portal/profile/page.tsx
@@ -0,0 +1,11 @@
+export default function ProfilePage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <h1 className="text-2xl font-bold">Profile Page</h1>
+      <p>
+        This is the profile page. You can view and edit your profile information
+        here.
+      </p>
+    </div>
+  );
+}
